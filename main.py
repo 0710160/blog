485dfd017c1f25192406bdb4b26c6c276e4ccede
0710160 <xlvi@mm.st>
2021-10-22 15:06:27 +1300

    Added editable form for new blog posts

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditor, CKEditorField
+from datetime import datetime
+import bleach
 
 
-## Delete this code:
-# import requests
-# posts = requests.get("https://api.npoint.io/43644ec4f0013682fc0d").json()
+def bleach_html(content):
+    '''
+    Strips html of any script injections
+    '''
+    allowed_tags = ['a', 'abbr', 'acronym', 'address', 'b', 'br', 'div', 'dl', 'dt',
+                    'em', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'i', 'img',
+                    'li', 'ol', 'p', 'pre', 'q', 's', 'small', 'strike',
+                    'span', 'sub', 'sup', 'table', 'tbody', 'td', 'tfoot', 'th',
+                    'thead', 'tr', 'tt', 'u', 'ul']
+
+    allowed_attrs = {
+        'a': ['href', 'target', 'title'],
+        'img': ['src', 'alt', 'width', 'height'],
+    }
+
+    cleaned = bleach.clean(content,
+                           tags=allowed_tags,
+                           attributes=allowed_attrs,
+                           strip=True)
+    return cleaned
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b1'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -38,22 +58,50 @@ class CreatePostForm(FlaskForm):
     subtitle = StringField("Subtitle", validators=[DataRequired()])
     author = StringField("Your Name", validators=[DataRequired()])
     img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
-    body = StringField("Blog Content", validators=[DataRequired()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
 
 
 @app.route('/')
 def get_all_posts():
+    posts = BlogPost.query.all()
     return render_template("index.html", all_posts=posts)
 
 
 @app.route("/post/<int:index>")
 def show_post(index):
-    requested_post = None
-    for blog_post in posts:
-        if blog_post["id"] == index:
-            requested_post = blog_post
+    #requested_post = None
+    #for blog_post in posts:
+    #    if blog_post["id"] == index:
+    #        requested_post = blog_post
+    requested_post = BlogPost.query.get(index)
     return render_template("post.html", post=requested_post)
+
+
+@app.route("/edit/<int:post_id>")
+def edit_post(post_id):
+    pass
+
+
+@app.route("/new_post", methods=["GET", "POST"])
+def new_post():
+    if request.method == "GET":
+        form = CreatePostForm()
+        return render_template("make-post.html", form=form)
+    if request.method == "POST":
+        ckeditor_output = request.form.get('body')
+        new_blog_post = BlogPost(
+            title=request.form['title'],
+            subtitle=request.form['subtitle'],
+            date=datetime.today().strftime('%m %d, %Y'),
+            author=request.form['author'],
+            img_url=request.form['img_url'],
+            body=bleach_html(ckeditor_output)
+        )
+        db.session.add(new_blog_post)
+        db.session.commit()
+        posts = BlogPost.query.all()
+        return render_template("index.html", all_posts=posts)
 
 
 @app.route("/about")
@@ -65,5 +113,7 @@ def about():
 def contact():
     return render_template("contact.html")
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    #app.run(host='10.0.1.26', port=5000)
+    app.run(debug=True)
